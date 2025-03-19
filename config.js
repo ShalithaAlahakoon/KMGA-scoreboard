1d7e271deba3a28d528ac866467c4d562428190b
Shalitha Alahakoon <shalitha@medfuture.com.au>
2025-03-19 21:33:49 +0530

    updates

---
+++ config.js
@@ -0,0 +1,4 @@
+const sheetLinks = {
+    WAG_12: "https://script.google.com/macros/s/AKfycbwCX58OcRwgyjDKuu93xERJ1B4Bpl-9JETlte3qqrrkAkG1YoMHsVnlfCSF-hGJgzLa/exec",
+    // Add more events if needed
+};
