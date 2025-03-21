6ca44855c3f1d616d8818c78af9593edfe6a527e
Shalitha Alahakoon <shalitha@medfuture.com.au>
2025-03-21 13:08:07 +0530

    mag 12 files

--- config.js
+++ config.js
@@ -1,4 +1,5 @@
 const sheetLinks = {
     WAG_12: "https://script.google.com/macros/s/AKfycbwCX58OcRwgyjDKuu93xERJ1B4Bpl-9JETlte3qqrrkAkG1YoMHsVnlfCSF-hGJgzLa/exec",
-    // Add more events if needed
+    MAG_12: "https://script.google.com/macros/s/AKfycbwl_Ols-ro4ML5YLb5Un4MUg5bPlr35S7DTL5H63Ho_lyrJ-WxNH1HxiPjH7S4uDg71/exec"
+
 };
