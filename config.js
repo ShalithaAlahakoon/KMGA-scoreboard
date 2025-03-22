abc4a7912fa81d0c6acebafcf364c248a0df889f
Shalitha Alahakoon <shalitha@medfuture.com.au>
2025-03-22 08:34:38 +0530

    wag12overall

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 const sheetLinks = {
-    WAG_12: "https://script.google.com/macros/s/AKfycbwCX58OcRwgyjDKuu93xERJ1B4Bpl-9JETlte3qqrrkAkG1YoMHsVnlfCSF-hGJgzLa/exec",
+    WAG_12: "https://script.google.com/macros/s/AKfycby2C7-XYY8ywKoYDMQpdZTSY-oN1VKVSAaqOtkkKEhrK8xMn2NezcBbYygv3h1XJwRm/exec",
     MAG_12: "https://script.google.com/macros/s/AKfycbwl_Ols-ro4ML5YLb5Un4MUg5bPlr35S7DTL5H63Ho_lyrJ-WxNH1HxiPjH7S4uDg71/exec"
 
 };
