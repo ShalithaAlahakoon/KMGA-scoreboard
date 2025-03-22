b1bf0c9233d0dd3ba2d5ea5a0d3769c793993743
Shalitha Alahakoon <shalitha@medfuture.com.au>
2025-03-22 10:04:39 +0530

    Male U12 All-Round Final

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 const sheetLinks = {
     WAG_12: "https://script.google.com/macros/s/AKfycbyZn_7ebw3LmtE_hlXdeY9nCyqafRMN1WiaZh0c0DC-1_i3pgJGop25PtbokmOg24w/exec",
-    MAG_12: "https://script.google.com/macros/s/AKfycbwl_Ols-ro4ML5YLb5Un4MUg5bPlr35S7DTL5H63Ho_lyrJ-WxNH1HxiPjH7S4uDg71/exec"
+    MAG_12: "https://script.google.com/macros/s/AKfycbwCLJc0i8Cwb8k3unc6tj8y_k0cxtOYqHXAC0qZ8WTWGN5R-sNCr7PdXyRayrTMwS4l/exec"
 
 };
