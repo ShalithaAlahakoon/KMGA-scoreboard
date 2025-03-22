42b1cb18ff4b6e284fd8919945624b337ce26ed7
Shalitha Alahakoon <shalitha@medfuture.com.au>
2025-03-22 11:52:51 +0530

    link update

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 const sheetLinks = {
     WAG_12: "https://script.google.com/macros/s/AKfycbyslqC8tr8e4t7aDc-N9F5Pg0sPAOIj42coDo3PMmX70Ih8j9yvPwWi1c6n8fPK0nuh/exec",
-    MAG_12: "https://script.google.com/macros/s/AKfycbwCLJc0i8Cwb8k3unc6tj8y_k0cxtOYqHXAC0qZ8WTWGN5R-sNCr7PdXyRayrTMwS4l/exec"
+    MAG_12: "https://script.google.com/macros/s/AKfycbzx7oR6q20DN6K-5_y4mCdrXpJ0tR_HeNDuSrYxLmwGVRib7FUsEghkDs8TWcaJ6oAe/exec"
 
 };
