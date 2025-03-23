b2c7201c931e066cb86ff36c46ff6d5c71cb5913
Shalitha Alahakoon <shalitha@medfuture.com.au>
2025-03-23 08:47:30 +0530

    male 10

--- config.js
+++ config.js
@@ -1,5 +1,6 @@
 const sheetLinks = {
     WAG_10: "https://script.google.com/macros/s/AKfycbxKOg3BVW_1X5oxGku9PUDSzhgLWsIcBC-LawNAMziR9xRyUpGAEYanQ6gpj36fM-aSTg/exec",
+    MAG_10: "https://script.google.com/macros/s/AKfycbxdaafEWS7waNQn9hVkIf6y1RVRYK0_A1LRZ5vDm5f7gtZfRZnGR8Qvx1uC2A4GqIk/exec",
     WAG_12: "https://script.google.com/macros/s/AKfycbxd-fZBC0Rh9nOhci4dX0PxnqhjKVSOrJV6--tRP183hIndjQz0gKc7b62f1-SQEHRw/exec",
     MAG_12: "https://script.google.com/macros/s/AKfycbzx7oR6q20DN6K-5_y4mCdrXpJ0tR_HeNDuSrYxLmwGVRib7FUsEghkDs8TWcaJ6oAe/exec"
 
